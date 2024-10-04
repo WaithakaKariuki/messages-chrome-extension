0888fe2b25ff7c986b15f69f96627381c4597562
NirvanaSatori <morris24kariuki@gmail.com>
2024-10-04 04:30:47 +0300

    create and loading component

---
+++ src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Loading() {
+  return (
+    <div className='flex justify-center' >
+      <span className='text-3xl animate-bounce'>
+         Loading...
+      </span>
+    </div>
+  )
+}
+
+export default Loading
