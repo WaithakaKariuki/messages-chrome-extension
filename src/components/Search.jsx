7feee90796fa7dde3da8c5cd7edf73a5a3ac9f13
NirvanaSatori <morris24kariuki@gmail.com>
2024-10-20 23:18:50 +0300

    add selectors for toggling darkMode manually in Search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -13,9 +13,9 @@ export default function Search({messageDispatch}) {
                     type="text"
                     name="search"
                     id="search"
-                    className="block w-full h-7 rounded-md border-2 border-gray-400 pl-4 pr-12 shadow-md focus:border-indigo-500 focus:ring-indigo-500 text-md"
+                    className="block w-full h-7 rounded-md border-2 border-gray-400 dark:border-gray-600 dark:bg-gray-800 dark:text-white pl-4 pr-12 shadow-md focus:border-indigo-500 focus:ring-indigo-500 text-md"
                 />
-            <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
+            <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5 dark:text-white">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                 </svg>
