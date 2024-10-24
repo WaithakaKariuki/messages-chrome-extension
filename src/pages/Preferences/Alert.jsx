40d8f7d44d81bcdbf2ab467298af7d3e639c8a03
NirvanaSatori <morris24kariuki@gmail.com>
2024-10-25 00:07:36 +0300

    add a dark variant to the Alert component

--- src/pages/Preferences/Alert.jsx
+++ src/pages/Preferences/Alert.jsx
@@ -1,35 +1,77 @@
-import { CheckCircleIcon } from '@heroicons/react/20/solid'
+import { Fragment, useState } from 'react'
+import { Transition } from '@headlessui/react'
+import { InboxIcon } from '@heroicons/react/24/outline'
+import { XMarkIcon } from '@heroicons/react/20/solid'
 
-export default function Alert() {
+export default function Alert({show, setShow}) {
+
+
   return (
-    <div className="rounded-md bg-green-50 p-4">
-      <div className="flex">
-        <div className="flex-shrink-0">
-          <CheckCircleIcon className="h-5 w-5 text-green-400" aria-hidden="true" />
-        </div>
-        <div className="ml-3">
-          <h3 className="text-sm font-medium text-green-800">Attention needed</h3>
-          <div className="mt-2 text-sm text-green-700">
-            <p>Once you mark all messages as read,  you cannot undo this action</p>
-          </div>
-          <div className="mt-4">
-            <div className="-mx-2 -my-1.5 flex">
-              <button
-                type="button"
-                className="rounded-md bg-green-50 px-2 py-1.5 text-sm font-medium text-green-800 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50"
-              >
-                Confirm
-              </button>
-              <button
-                type="button"
-                className="ml-3 rounded-md bg-green-50 px-2 py-1.5 text-sm font-medium text-green-800 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50"
-              >
-                Cancel
-              </button>
+    <>
+      {/* Global notification live region, render this permanently at the end of the document */}
+      <div
+        aria-live="assertive"
+        className="pointer-events-none fixed inset-0 flex items-end -px-2 -mx-2 my-10 sm:items-start sm:p-2"
+      >
+        <div className="flex w-full flex-col items-start space-y-4 sm:items-start">
+          {/* Notification panel, dynamically insert this into the live region when it needs to be displayed */}
+          <Transition
+            show={show}
+            as={Fragment}
+            enter="transform ease-out duration-300 transition"
+            enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+            enterTo="translate-y-0 opacity-100 sm:translate-x-0"
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white dark:bg-gray-900 shadow-lg ring-1 ring-black ring-opacity-5">
+              <div className="p-1">
+                <div className="flex items-start">
+                  <div className="flex-shrink-0">
+                    <InboxIcon className="h-6 w-6 text-gray-400" aria-hidden="true" />
+                  </div>
+                  <div className="ml-1 w-0 flex-1 pt-0.5">
+                    <p className="text-sm font-medium text-gray-900">Attention needed</p>
+                    <p className="mt-1 text-sm text-gray-500 tracking tight">
+                    Once you mark all messages as read,  you cannot undo this action
+                    </p>
+                    <div className="mt-4 flex space-x-7">
+                      <button
+                        type="button"
+                        className="rounded-md bg-white dark:bg-gray-900 text-sm font-medium text-indigo-600 dark:text-indigo-400 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                      >
+                        Confirm
+                      </button>
+                      <button
+                        onClick={() => {
+                            setShow(false)
+                        }}
+                        type="button"
+                        className="rounded-md bg-white dark:bg-gray-900 text-sm font-medium text-gray-700 dark:text-gray-300  hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  </div>
+                  <div className="ml-4 flex flex-shrink-0">
+                    <button
+                      type="button"
+                      className="inline-flex rounded-md bg-white dark:bg-gray-900 text-gray-400 dark:text-gray-300 hover:text-gray-500 dark:hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                      onClick={() => {
+                        setShow(false)
+                      }}
+                    >
+                      <span className="sr-only">Close</span>
+                      <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
+          </Transition>
         </div>
       </div>
-    </div>
+    </>
   )
 }
--- src/pages/Preferences/ReadAllMessages.jsx
+++ src/pages/Preferences/ReadAllMessages.jsx
@@ -1,28 +1,42 @@
+import { useState } from "react"
+import Alert from "./Alert"
+import { CheckIcon, EnvelopeIcon } from "@heroicons/react/24/solid"
+
 export default function ReadAllMessages() {
+    const [show, setShow] = useState(false)
     return (
-      <fieldset className="border-t border-b border-gray-200 px-4 bg-gray-100 bg-gray-200 dark:bg-gray-800 border-t">
-        <legend className="sr-only">Mark all messages as read</legend>
-        <div className="divide-y divide-gray-200">
-          <div className="relative flex items-start py-4">
-            <div className="min-w-0 flex-1 text-lg">
-              <label htmlFor="comments" className="font-medium text-gray-900 dark:text-white tracking-tight">
-              Mark all messages as read
-              </label>
-              <p id="comments-description" className="tracking-tighter text-xs text-gray-300">
-                This action cannot be undone!
-              </p>
-            </div>
-            <div className="ml-3 flex h-5 items-center">
-              <input
-                id="comments"
-                aria-describedby="comments-description"
-                name="comments"
-                type="checkbox"
-                className="h-4.5 w-4.5 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-              />
-            </div>
-          </div>
-        </div>
-      </fieldset>
+        <>
+            {show? 
+                (<Alert show={show} setShow={setShow} />)
+            :
+            (    <fieldset className="border-t border-b border-gray-200 px-4 bg-gray-100 bg-gray-200 dark:bg-gray-800 border-t">
+                    <legend className="sr-only">Mark all messages as read</legend>
+                    <div className="divide-y divide-gray-200">
+                    <div className="relative flex items-start py-4">
+                        <div className="min-w-0 flex-1 text-lg">
+                        <label htmlFor="comments" className="font-medium text-gray-900 dark:text-white tracking-tight">
+                        Mark all messages as read
+                        </label>
+                        <p id="comments-description" className="tracking-tighter text-xs text-gray-300">
+                            This action cannot be undone!
+                        </p>
+                        </div>
+                        <div className="ml-3 flex h-5 items-center self-center ">
+                        <button
+                            onClick={() => {
+                                setShow(true)
+                              }}
+                            type="button"
+                            className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-3 py-2 text-sm font-medium leading-4 text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                        >
+                            <CheckIcon className="-ml-0.5 mr-2 h-4 w-4" aria-hidden="true" />
+                            Confirm
+                        </button>
+                        </div>
+                    </div>
+                    </div>
+                </fieldset>)
+            }
+      </>
     )
   }
