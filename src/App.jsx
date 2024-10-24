96a23d73292fe1c733898c0a4536ff16b479d422
NirvanaSatori <morris24kariuki@gmail.com>
2024-10-24 22:49:22 +0300

    move the useReducer hook function to the Home folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,19 @@
 /*global chrome*/
-import { useReducer } from 'react'
 import './App.css'
-import List from './pages/Home/List'
-import Navbar from './pages/Home/Navbar'
-import { messageReducer } from './utils/Reducer';
-import Search from './pages/Home/Search';
 import { Route, Routes, BrowserRouter} from "react-router-dom";
 import Preferences from './pages/Preferences/Preferences';
 import Home from './pages/Home/Home';
 
 function App() {
   // chrome.action.setBadgeText({ text: '' });
-  console.log('badge removed successfully');
-  const [messageState, messageDispatch] = useReducer(messageReducer, {
-    sort:"",
-    search:""
-  });
 
   return (
     <>
       <div className='max-w-sm min-w-96 dark'>
         <BrowserRouter>
-          <Home messageState={messageState} messageDispatch={messageDispatch} />
           <Routes>
             <Route path="/preferences" element={<Preferences/>} />
+            <Route path="/home" element={<Home/>} />
           </Routes>
         </BrowserRouter>
       </div>  
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import List from './List'
 import Navbar from './Navbar'
 import Search from './Search'
+import { messageReducer } from '../../utils/Reducer';
 
 function Home({messageState, messageDispatch}) {
+    const [messageState, messageDispatch] = useReducer(messageReducer, {
+        sort:"",
+        search:""
+      });
   return (
     <>
         <Navbar messageDispatch={messageDispatch}/>
